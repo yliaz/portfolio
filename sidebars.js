d861bc91b314332e1ce067cb4e30c741890e9ae1
zhuye <zhuye256@163.com>
2021-11-15 23:48:30 +0800

    侧边栏分组默认展开

--- sidebars.js
+++ sidebars.js
@@ -17,21 +17,25 @@ const sidebars = {
     {
       type: 'category',
       label: 'CSS',
+      collapsible: false,
       items: ['blog/example-01'],
     },
     {
       type: 'category',
       label: 'JavaScript',
+      collapsible: false,
       items: ['blog/example-01'],
     },
     {
       type: 'category',
       label: '算法',
+      collapsible: false,
       items: ['blog/algorithm/unicode-to-utf-8'],
     },
     {
       type: 'category',
       label: '杂七杂八',
+      collapsible: false,
       items: ['blog/example-01'],
     },
   ],
@@ -39,11 +43,13 @@ const sidebars = {
     {
       type: 'category',
       label: 'M1 Macbook 开发环境配置',
+      collapsible: false,
       items: ['experience/m1-macbook-install-homebrew'],
     },
     {
       type: 'category',
       label: '使用 Docusaurus 构建个人主页',
+      collapsible: false,
       items: [
         'experience/portfolio-buy-domain',
         'experience/portfolio-buy-server',
@@ -52,6 +58,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Notion 使用技巧',
+      collapsible: false,
       items: ['experience/notion-custom-domain'],
     },
   ],
@@ -62,11 +69,13 @@ const sidebars = {
     {
       type: 'category',
       label: '前端面试刷题',
+      collapsible: false,
       items: ['interview/example-04'],
     },
     {
       type: 'category',
       label: '英语面试技巧',
+      collapsible: false,
       items: ['interview/example-04'],
     },
   ],
@@ -77,16 +86,19 @@ const sidebars = {
     {
       type: 'category',
       label: 'ES6 标准入门',
+      collapsible: false,
       items: ['reading-note/example-06'],
     },
     {
       type: 'category',
       label: '图解 HTTP',
+      collapsible: false,
       items: ['reading-note/example-06'],
     },
     {
       type: 'category',
       label: 'CSS 揭秘',
+      collapsible: false,
       items: ['reading-note/example-06'],
     },
   ]
