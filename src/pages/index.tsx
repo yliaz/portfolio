2aa055413b826d9c6dd1e498de1b1c6a5fd9d770
zhuye <zhuye256@163.com>
2021-11-28 04:25:03 +0800

    add gsap react-gsap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,26 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import Homepage from '@site/src/components/Homepage/Homepage';
 
-// function HomepageHeader() {
-//   const {siteConfig} = useDocusaurusContext();
-//   return (
-//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-//       <div className="container">
-//         <h1 className={clsx("hero__title", styles.heroTitle)}>{siteConfig.title}</h1>
-//         <p className="hero__subtitle">{siteConfig.tagline}</p>
-//       </div>
-//     </header>
-//   );
-// }
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className={clsx("hero__title", styles.heroTitle)}>{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Homepage />
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+    </Layout>
   );
 }
---
+++ src/pages/me/index.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react';
+
+
+const Me: FC = () => {
+  return <div>
+
+  </div>
+}
+
+export default Me;
