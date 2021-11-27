a674c53848e54aa1696ea0699034f3799ee0a2e1
zhuye <zhuye256@163.com>
2021-11-28 07:40:44 +0800

    change homepage icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
+import Logo from '@site/static/img/logo.svg'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <Logo style={{ maxHeight: 120, marginBottom: 20 }} />
         <h1 className={clsx("hero__title", styles.heroTitle)}>{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
