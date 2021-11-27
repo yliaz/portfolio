3a170739dcacdb50cbf4c48757242fc87b9767ef
zhuye <zhuye256@163.com>
2021-11-28 07:06:01 +0800

    change logo icon

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,9 +1,26 @@
 import React, {FC} from 'react';
-
+import {ScrollTrigger, Tween} from 'react-gsap';
 
 const Me: FC = () => {
   return <div>
-
+    <div style={{ height: 1000 }}>aaa</div>
+    <ScrollTrigger start="-200px center" end="200px center" scrub={0.5}>
+      <Tween
+        to={{
+          x: '300px',
+        }}
+      >
+        <div style={{ width: '100px', height: '100px', background: '#ccc' }} />
+      </Tween>
+      <Tween
+        to={{
+          x: '300px',
+        }}
+      >
+        <div style={{ width: '100px', height: '100px', background: '#999' }} />
+      </Tween>
+    </ScrollTrigger>
+    <div style={{ height: 1000 }}>aaa</div>
   </div>
 }
 
